0d9c0912a9112f32ca0d3cb919b5be16ea06d07b
Taekhwan Han <hth3396@gmail.com>
2023-04-17 19:56:25 +0900

    feat: 테스트 구문 삭제

--- main.py
+++ main.py
@@ -17,14 +17,13 @@ def jsonParser(data):
 
 # 식당 메뉴 정보 가져오는 함수
 def getMenuInfo(mealSchedule) :
-    print("startGetMenuInfo()")
     menuInfoDict = {}
     for cafeteriaIndex in range(1, 10) :
         menuCount = 0
         try :
             cafeteriaName = dr.find_element(By.CSS_SELECTOR, '#carteP005 > li > dl:nth-child('+ str(cafeteriaIndex) +') > dt').text
             menuInfoDict[cafeteriaName] = {}
-            if cafeteriaIndex != 1 : #첫번째 식당의 경우 이미 선택되어있으므로 클릭하지 않고 넘어감
+            if cafeteriaIndex != 1 :
                 getcafeteria = dr.find_element(By.CSS_SELECTOR, '#carteP005 > li > dl:nth-child('+ str(cafeteriaIndex) +') > dt > a')
                 getcafeteria.click()
                 time.sleep(0.5)
@@ -53,7 +52,6 @@ def getMenuInfo(mealSchedule) :
 
 # 데일리 메뉴 정보 가져오는 함수
 def getDailyMenu() :
-    print("startGetDailyMenu()")
     dailyMenuInfoDict = {}
     for mealSchedule in range(1, 4) :
         getMealSchedule = dr.find_element(By.CSS_SELECTOR, '#P005 > div > div > div > div > ol > li > header > div.nb-right.nb-t-right > ol > li:nth-child('+ str(mealSchedule) +')')
@@ -66,15 +64,14 @@ def getDailyMenu() :
 # 위클리 메뉴 정보 가져오는 함수
 def getWeekOfMealMenu() :
     weeklyMenuDict = {}
-    weeklyIndex = 1
-    for campus in range(1, 2):
+    weeklyIndex = 7
+    for campus in range(1, 3):
         weeklyMenuDict[campus-1] = {}
         for day in range(weeklyIndex) :
             getCampus = dr.find_element(By.CSS_SELECTOR, '#P005 > div > div > div > div > header > div > ol > li:nth-child(' + str(campus) + ') > span')
-            getCampus.click() #setCampus 메소드
+            getCampus.click()
             time.sleep(0.5)
             getDay = dr.find_element(By.CSS_SELECTOR, '#P005 > div > div > div > div > ol > li > header > div.nb-left > div > p')
-            print("getDailyMenu()")
             weeklyMenuDict[campus-1][getDay.text] = getDailyMenu()
             time.sleep(0.3)
             setNextDay = dr.find_element(By.CSS_SELECTOR, '#P005 > div > div > div > div > ol > li > header > div.nb-left > div > a.nb-p-time-select-next').click()
