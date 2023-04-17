3c1daa083b22f209c0a6ad0d9d8364e0c5b551bd
Taekhwan Han <hth3396@gmail.com>
2023-04-17 14:22:37 +0900

    feat: 메뉴 수에 따른 예외처리 및 테스트 수정

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./crawler/hasikServiceAcountKey.json"
 
 
-def jsonParser(data):
-    with open(os.path.join(BASE_DIR, 'CAU_Cafeteria_Menu.json'), 'w+', encoding='utf-8') as f :
-        json.dump(data, f, ensure_ascii=False, indent='\t')
+# def jsonParser(data):
+#     with open(os.path.join(BASE_DIR, 'CAU_Cafeteria_Menu.json'), 'w+', encoding='utf-8') as f :
+#         json.dump(data, f, ensure_ascii=False, indent='\t')
 
 
 
 # 식당 메뉴 정보 가져오는 함수
-def getMenuInfo() :
+def getMenuInfo(mealSchedule) :
+    print("startGetMenuInfo()")
     menuInfoDict = {}
     for cafeteriaIndex in range(1, 10) :
+        menuCount = 0
         try :
             cafeteriaName = dr.find_element(By.CSS_SELECTOR, '#carteP005 > li > dl:nth-child('+ str(cafeteriaIndex) +') > dt').text
             menuInfoDict[cafeteriaName] = {}
@@ -25,6 +27,7 @@ def getMenuInfo() :
                 getcafeteria.click()
                 time.sleep(0.5)
             for cafeteriaInfoIndex in range(2, 30) :
+                menuCount += 1
                 try :
                     getcafeteriaInfo = dr.find_element(By.CSS_SELECTOR, '#carteP005 > li > dl:nth-child('+ str(cafeteriaIndex) +') > dd:nth-child('+ str(cafeteriaInfoIndex) +')')
                     MenuType = dr.find_element(By.CSS_SELECTOR, '#carteP005 > li > dl:nth-child('+ str(cafeteriaIndex) +') > dd:nth-child('+ str(cafeteriaInfoIndex) +') > label > ul > li:nth-child(2) > span').text
@@ -41,19 +44,21 @@ def getMenuInfo() :
                     menuInfoDict[cafeteriaName][MenuType]['menu'] = mealInfo.replace('\n', '|')
                 except :
                     pass
+                if cafeteriaName != "학생식당(303관B1층)" and menuCount == 5 : break
         except :
             pass
     return menuInfoDict
 
 # 데일리 메뉴 정보 가져오는 함수
 def getDailyMenu() :
+    print("startGetDailyMenu()")
     dailyMenuInfoDict = {}
     for mealSchedule in range(1, 4) :
         getMealSchedule = dr.find_element(By.CSS_SELECTOR, '#P005 > div > div > div > div > ol > li > header > div.nb-right.nb-t-right > ol > li:nth-child('+ str(mealSchedule) +')')
         dailyMenuInfoDict[mealSchedule-1] = {}
         getMealSchedule.click()
         time.sleep(0.5)
-        dailyMenuInfoDict[mealSchedule-1] = getMenuInfo()
+        dailyMenuInfoDict[mealSchedule-1] = getMenuInfo(mealSchedule)
     return dailyMenuInfoDict
 
 # 위클리 메뉴 정보 가져오는 함수
@@ -68,6 +73,7 @@ def getWeekOfMealMenu() :
             getCampus.click() #setCampus 메소드
             time.sleep(0.5)
             getDay = dr.find_element(By.CSS_SELECTOR, '#P005 > div > div > div > div > ol > li > header > div.nb-left > div > p')
+            print("getDailyMenu()")
             weeklyMenuDict[campus-1][getDay.text] = getDailyMenu()
             time.sleep(0.3)
             setNextDay = dr.find_element(By.CSS_SELECTOR, '#P005 > div > div > div > div > ol > li > header > div.nb-left > div > a.nb-p-time-select-next').click()
@@ -79,15 +85,15 @@ def getWeekOfMealMenu() :
 
 def runCrawler():
 #    jsonParser(getWeekOfMealMenu())
-    getWeekOfMealMenu()
+    print(getWeekOfMealMenu())
     print("크롤링 완료")
 
 try :
     options = webdriver.ChromeOptions()
     options.add_argument("start-maximized")
     options.add_argument("lang=ko_KR")
-    options.add_argument('headless')
-    options.add_argument("--no-sandbox")
+    #options.add_argument('headless')
+    #options.add_argument("--no-sandbox")
 
     # chrome driver
     dr = webdriver.Chrome('chromedriver', chrome_options=options)
