eeca6419036c747fa84f091dffeaa4cf12de1c3d
Taekhwan Han <hth3396@gmail.com>
2023-04-21 15:42:10 +0900

    feat: TestCode 작성

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def getMenuInfo(mealSchedule) :
 # 데일리 메뉴 정보 가져오는 함수
 def getDailyMenu() :
     dailyMenuInfoDict = {}
-    for mealSchedule in range(1, 4) :
+    for mealSchedule in range(1, 3) :
         getMealSchedule = dr.find_element(By.CSS_SELECTOR, '#P005 > div > div > div > div > ol > li > header > div.nb-right.nb-t-right > ol > li:nth-child('+ str(mealSchedule) +')')
         dailyMenuInfoDict[mealSchedule-1] = {}
         getMealSchedule.click()
@@ -65,8 +65,8 @@ def getDailyMenu() :
 # 위클리 메뉴 정보 가져오는 함수
 def getWeekOfMealMenu() :
     weeklyMenuDict = {}
-    weeklyIndex = 7
-    for campus in range(1, 3):
+    weeklyIndex = 1
+    for campus in range(1, 2):
         weeklyMenuDict[campus-1] = {}
         for day in range(weeklyIndex) :
             getCampus = dr.find_element(By.CSS_SELECTOR, '#P005 > div > div > div > div > header > div > ol > li:nth-child(' + str(campus) + ') > span')
